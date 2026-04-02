3342f5a7ff06f5d2c68cdce28926d869ab55fcdd
Sam LeRose <samuel.lerose@gmail.com>
2026-04-01 23:24:42 -0400

    Update CMakeList items and add cluster script

---
+++ NeutronActivation/run_nAct.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time 48:00:00         # Time limit for the job (REQUIRED)
+#SBATCH --job-name=Neutron_Activation        # Job name
+#SBATCH --ntasks=8              # Number of cores to allocate. Same as SBATCH -n
+#SBATCH --partition=normal      # Partition/queue to run the job in. (REQUIRED)
+#SBATCH -e slurm-%j.err         # Error file for this job.
+#SBATCH -o slurm-%j.out         # Output file for this job.
+#SBATCH -A NAME                 # Project allocation account name (REQUIRED)
+#SBATCH --mail-type ALL         # Send email when job starts/ends
+#SBATCH --mail-user EMAIL_ADDRESS  # Email address to send notifications to
+
+singularity run --app geant41132root6344 /share/singularity/images/ccs/conda/amd-conda26-rocky9.sinf ../NeutronActivation run.mac
